ea4ddf9a4ff9a9e10796f834e8d2f2646f39e07f
notnotnotswipez <strubb06@gmail.com>
2026-04-24 21:29:16 -0400

    1.14.0 Various BushWolf patches

--- MoreCompany/EnemyAIPatches.cs
+++ MoreCompany/EnemyAIPatches.cs
@@ -1,12 +1,31 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Reflection.Emit;
+using GameNetcodeStuff;
 using HarmonyLib;
 using UnityEngine;
 
 namespace MoreCompany
 {
+
+    public static class InstructionUtils {
+
+        public static bool DoesSafeMatchInstruction(List<CodeInstruction> instructions, int index, OpCode opCode) {
+            if (index >= instructions.Count) {
+                return false;
+            }
+
+            CodeInstruction codeInstruction = instructions[index];
+            if (codeInstruction.opcode != opCode) {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
 	[HarmonyPatch(typeof(ForestGiantAI), "LookForPlayers")]
 	public static class LookForPlayersForestGiantPatch
 	{
@@ -106,7 +125,85 @@ namespace MoreCompany
         }
 	}
 
-	[HarmonyPatch(typeof(EnemyAI), "GetAllPlayersInLineOfSight")]
+    [HarmonyPatch(typeof(BushWolfEnemy), nameof(BushWolfEnemy.Update))]
+    public static class BushWolfEnemyPlayerObservePatch
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> codeInstructions = instructions.ToList();
+            FieldInfo livingPlayersField = typeof(StartOfRound).GetField(nameof(StartOfRound.livingPlayers), BindingFlags.Instance | BindingFlags.Public);
+            MethodInfo mathfClamp = AccessTools.Method(typeof(Mathf), nameof(Mathf.Clamp), new []{typeof(int), typeof(int), typeof(int)});
+
+            bool didPatch = false;
+
+            for (int i = 0; i < codeInstructions.Count; i++) {
+                CodeInstruction activeInstruction = codeInstructions[i];
+                if (activeInstruction.opcode == OpCodes.Ldfld && (FieldInfo)activeInstruction.operand == livingPlayersField) {
+                    if (InstructionUtils.DoesSafeMatchInstruction(codeInstructions, i + 1, OpCodes.Ldc_I4_2) && InstructionUtils.DoesSafeMatchInstruction(codeInstructions, i + 2, OpCodes.Sub)) {
+                        // Remove the ldc.i4.2 and sub instruction
+                        codeInstructions.RemoveAt(i + 1);
+                        codeInstructions.RemoveAt(i + 1);
+
+                        // Insert at i+2 (Skipping the ldc.i4.0) the ldc.i4.3
+                        codeInstructions.Insert(i+2, new CodeInstruction(OpCodes.Ldc_I4_3));
+
+                        // Replace the method call from max to clamp
+                        codeInstructions[i + 3].operand = mathfClamp;
+                        didPatch = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!didPatch) MainClass.StaticLogger.LogWarning("BushWolfEnemy.Update failed to patch observation rule (Max 3 observers)");
+
+            return codeInstructions;
+        }
+    }
+
+    [HarmonyPatch(typeof(BushWolfEnemy), nameof(BushWolfEnemy.GetClosestPlayerToNest))]
+    public static class BushWolfEnemyClosestPlayerNestPatch
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var newInstructions = new List<CodeInstruction>();
+            bool alreadyReplaced = false;
+            foreach (var instruction in instructions)
+            {
+                if (!alreadyReplaced)
+                {
+                    if (instruction.ToString() == "ldc.i4.4 NULL")
+                    {
+                        alreadyReplaced = true;
+                        CodeInstruction codeInstruction = new CodeInstruction(OpCodes.Ldsfld, AccessTools.Field(typeof(MainClass), "newPlayerCount"));
+                        newInstructions.Add(codeInstruction);
+                        continue;
+                    }
+                }
+
+                newInstructions.Add(instruction);
+            }
+
+            if (!alreadyReplaced) MainClass.StaticLogger.LogWarning("GetClosestPlayerToNest failed to replace newPlayerCount");
+
+            return newInstructions.AsEnumerable();
+        }
+    }
+
+    [HarmonyPatch(typeof(BushWolfEnemy), nameof(BushWolfEnemy.DoAIInterval))]
+    public static class BushWolfTargetDelayPatch {
+        public static void Prefix(BushWolfEnemy __instance, ref int ___checkPlayer) {
+            PlayerControllerB currentPlayerCheck = StartOfRound.Instance.allPlayerScripts[___checkPlayer];
+            int iterations = 0;
+            while ((!currentPlayerCheck.isPlayerControlled || currentPlayerCheck.isPlayerDead) && iterations < StartOfRound.Instance.allPlayerScripts.Length) {
+                ___checkPlayer = (___checkPlayer + 1) % StartOfRound.Instance.allPlayerScripts.Length;
+                currentPlayerCheck = StartOfRound.Instance.allPlayerScripts[___checkPlayer];
+                iterations++;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(EnemyAI), "GetAllPlayersInLineOfSight")]
     public static class GetAllPlayersInLineOfSightPatch
     {
         public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
